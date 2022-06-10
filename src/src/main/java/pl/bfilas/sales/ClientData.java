5aec3e56dd13b8bd0b3caf6a19613d2caadede0d
Barthezzone1 <s219117@student.uek.kakow.pl>
2022-06-10 23:07:20 +0200

    describe ordering scenario

---
+++ src/src/main/java/pl/bfilas/sales/ClientData.java
@@ -0,0 +1,11 @@
+package pl.bfilas.sales;
+
+import lombok.*;
+
+@Data
+@Builder
+public class ClientData {
+    String firstname;
+    String lastname;
+    String email;
+}
---
+++ src/src/main/java/pl/bfilas/sales/PaymentData.java
@@ -0,0 +1,9 @@
+package pl.bfilas.sales;
+
+import lombok.Data;
+
+@Data
+public class PaymentData {
+    String paymentId;
+    String url;
+}
